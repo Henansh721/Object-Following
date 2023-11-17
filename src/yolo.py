7e442b191db27e442a3e7235cf0d2d26dc6e65c1
Henansh721 <88682260+Henansh721@users.noreply.github.com>
2023-11-18 01:03:47 +0530

    updated

--- src/yolo.py
+++ src/yolo.py
@@ -1,9 +1,10 @@
 from COCO import coco_classes
 import cv2
 from ultralytics import YOLO
+import numpy as np
 
 OBJECT = 'Person'
-TOLERENCE = 20
+TOLERANCE = 20
 OBJECT_SIZE = 470
 
 # Style Parameters
@@ -14,14 +15,14 @@ FONT_THICKNESS = 1
 RADIUS = 1
 THICKNESS = 5
 
-def takeAction(x, size, size_desired, x_tolerence=TOLERENCE, size_tolerence=TOLERENCE):
+def takeAction(x, size, size_desired, x_tolerance=TOLERANCE, size_tolerance=TOLERANCE):
     text = ""
-    if abs(size - size_desired) > size_tolerence:
-        if(size > size_desired):
+    if abs(size - size_desired) > size_tolerance:
+        if size > size_desired:
             text += "Move Backward "
-        elif(size < size_desired):
+        elif size < size_desired:
             text += "Move Forward "
-    if abs(x) > x_tolerence:
+    if abs(x) > x_tolerance:
         if x < 0:
             text += "Rotate Left"
         elif x > 0:
@@ -31,13 +32,10 @@ def takeAction(x, size, size_desired, x_tolerence=TOLERENCE, size_tolerence=TOLE
     return text
 
 if __name__ == '__main__':
-
     model = YOLO('yolov8n.pt')
-
+    
+    # OpenCV VideoCapture for camera
     cap = cv2.VideoCapture(0)
-    ret, image = cap.read()
-
-    centre_x, centre_y = image.shape[1]/2, image.shape[0]/2
 
     # Check if the camera opened successfully
     if not cap.isOpened():
@@ -53,57 +51,39 @@ if __name__ == '__main__':
             print("Error: Could not read frame.")
             break
         
-        results = model.predict(image, classes = [coco_classes[OBJECT]])
+        results = model.predict(image, classes=[coco_classes[OBJECT]])
 
-        # for i in range(len(results[0].boxes.xyxy)):
-        # result = results[0].boxes.xyxy[i]
-        
-        if len(results[0].boxes.xyxy > 0):
+        if len(results[0].boxes.xyxy) > 0:
             objectFound = True
         else:
             objectFound = False
 
         # If object is present
-        if objectFound == True:    
+        if objectFound:
             result = results[0].boxes.xyxy[0]
 
             x_mid = (result[0] + result[2]) / 2
             y_mid = (result[1] + result[3]) / 2
 
-            x_deviation = x_mid - centre_x
-            y_deviation = centre_y - y_mid
+            x_deviation = x_mid - (image.shape[1] / 2)
+            y_deviation = (image.shape[0] / 2) - y_mid
             size = result[2] - result[0]
             action = takeAction(x_deviation, size, OBJECT_SIZE)
-            print("Action",action, f"X: {int(x_deviation)}", (0, int(centre_y)*2-5), f"Y: {int(y_deviation)}", (100, int(centre_y)*2-5), f"Size: {int(size)}", (200, int(centre_y)*2-5))
+            print("Action", action, f"X: {int(x_deviation)}", f"Y: {int(y_deviation)}", f"Size: {int(size)}")
 
-            # Bounding Box
-        #     cv2.rectangle(image, (int(result[0]), int(result[1])), (int(result[2]), int(result[3])), 0, 4)
+            # Display the result on the image
+            cv2.putText(image, f"X: {int(x_deviation)}", (0, image.shape[0] - 5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
+            cv2.putText(image, f"Y: {int(y_deviation)}", (100, image.shape[0] - 5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
+            cv2.putText(image, f"Size: {int(size)}", (200, image.shape[0] - 5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
+            cv2.putText(image, "Action: " + action, (int(image.shape[1] / 2), image.shape[0] - 5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
 
-        #     # Object Centre
-        #     cv2.circle(image, (int(x_mid), int(y_mid)), RADIUS, (0, 255, 0), THICKNESS)
-
-        #     # Texts
-        #     cv2.rectangle(image, (0, int(centre_y)*2-30), (int(centre_x)*2, int(centre_y)*2), (255, 255, 255), -1)
-        #     cv2.putText(image, f"X: {int(x_deviation)}", (0, int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        #     cv2.putText(image, f"Y: {int(y_deviation)}", (100, int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        #     cv2.putText(image, f"Size: {int(size)}", (200, int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        #     cv2.putText(image, "Action: " + action, (int(centre_x), int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        
-        # # Frame centre
-        # cv2.circle(image, (int(centre_x), int(centre_y)), RADIUS, (0, 0, 255), THICKNESS)
-        
-        # # Texts
-        # if objectFound == False:
-        #     cv2.rectangle(image, (0, int(centre_y)*2-30), (int(centre_x)*2, int(centre_y)*2), (255, 255, 255), -1)
-        #     cv2.putText(image, f"X: -", (0, int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        #     cv2.putText(image, f"Y: -", (100, int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        #     cv2.putText(image, f"Size: -", (200, int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        #     cv2.putText(image, "Action: -", (int(centre_x), int(centre_y)*2-5), FONT, FONT_SCALE, COLOR, FONT_THICKNESS) 
-        
-
-        # # Display the frame
-        # cv2.imshow('Video', image)
+        # Display the image
+        cv2.imshow('Camera Feed', image)
 
         # Press 'q' to exit the loop
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+    # Release the camera
+    cap.release()
+    cv2.destroyAllWindows()
